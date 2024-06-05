631376abf0383c61187886cfdc1532f63835d352
dressrahmat <dedysrahmat97@gmail.com>
2024-06-05 13:38:22 +0700

    perbaikan di app. js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+const App = () =>  {
+
+  return (
+    <SafeAreaView>
+      <Text>Hello Dedy</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
